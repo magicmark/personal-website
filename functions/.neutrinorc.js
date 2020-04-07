996df0018263d26ea741335e3562ef85dd1e08f9
Mark Larah <markl@yelp.com>
2020-04-07 03:41:52 -0700

    update functions

--- functions/.neutrinorc.js
+++ functions/.neutrinorc.js
@@ -2,8 +2,6 @@ const node = require('@neutrinojs/node');
 module.exports = {
     use: [node()],
     options: {
-        source: 'functions_src',
-        output: 'functions_dist',
         mains: { hello: 'hello' },
     },
 };
